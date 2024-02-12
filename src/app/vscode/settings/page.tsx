eab8dfdb1483cfdf17fae84e85b354c39bef863a
Davi Moreira <davimoreira@MacBook-Air-de-Davi.local>
2024-02-11 22:02:24 -0300

    Fix error ts

--- src/app/vscode/settings/page.tsx
+++ src/app/vscode/settings/page.tsx
@@ -5,9 +5,6 @@ export const metadata = {
 };
 
 export default async function Settings() {
-  {
-    /* @ts-expect-error Server Component */
-  }
   return (
     <GistContent gistUrl="https://gist.githubusercontent.com/davimoreira24/a963afd15238219d6dad46955cea5f97/raw/ff8120a86b9c572b78713c7ac4c2af4168bac11a/settings.json" />
   );
